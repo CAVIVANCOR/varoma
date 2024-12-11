fd1df7a6b0db262df26b8c299c5a522c52abeeae
CARLOS ALBERTO VIVANCO RODRIGUEZ <SISTEMAS.CAVR@GMAIL.COM>
2024-12-11 16:43:55 -0500

    Confirmacion creacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
-      Hola Mundo Soy Carlos...!
+      Hola Mundo Soy Carlos Vivanco...!
     </h1>
   )
 }
